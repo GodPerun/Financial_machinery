45a10ff77ca7a31594a587fd00189e15094d4f78
hladik <xhladi02@gmail.com>
2021-02-07 23:02:20 +0100

    a

---
+++ libs/knihovna.py
@@ -0,0 +1,7 @@
+
+def print_hi(name):
+    print(__name__)
+    print("tu je name")
+    print(dir(__name__))
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # This is a sample Python script.
-
+from libs.knihovna import print_hi
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def return_43():
+    print("--------------")
+    print(dir())
+    print("--------------")
+    print(globals())
+    print("--------------")
+    print(locals())
+    print("--------------")
+    return 5
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyChaa')
+    print(print_hi.__dir__())
+    return_43()
+    print(__name__)
 
+
+exit(2)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
